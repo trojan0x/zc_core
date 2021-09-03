93f51db7267c8f32ad039eecd7237822118e41c6
trojan0X <jefdauda@gmail.com>
2021-09-03 13:19:55 +0100

    reverted changes on the example.env

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main() {
 
 	// load .env file if it exists
 
-	err := godotenv.Load("example.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
