16494b55079368cd35022e56fda018c0f0591b6f
trojan0X <jefdauda@gmail.com>
2021-09-03 11:34:07 +0100

    started working on  the verify endpoint to verify a user

--- user/verify.go
+++ user/verify.go
@@ -1,1 +1,36 @@
 package user
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"zuri.chat/zccore/utils"
+)
+
+func verify(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	user_collection := "users"
+	var user User
+
+	err := utils.ParseJsonFromRequest(request, &user)
+	if err != nil {
+		utils.GetError(err, http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	if !utils.IsValidEmail(user.Email) {
+		utils.GetError(errors.New("email address is not valid"), http.StatusBadRequest, response)
+		return
+	}
+
+	// confirm if user_email exists
+	result, _ := utils.GetMongoDbDoc(user_collection, bson.M{"email": user.Email})
+	if result != nil {
+		fmt.Printf("users with email %s exists!", user.Email)
+		utils.GetError(errors.New("operation failed"), http.StatusBadRequest, response)
+		return
+	}
+
+}
